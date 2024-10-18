297df49464688f5db278c926523c0f760b2863f1
Raja <raja.rathinam@pcisage.com>
2024-10-18 13:22:06 +0530

    gallery screen

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import Link from "next/link";
 
-const Layout = ({ children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <Header  />
+      <Header />
+
       {children}
-      <Footer  />
+      <Footer />
+      <div>
+        <br />
+        {/* <Link
+          target="_blank"
+          href="upi://pay?pa=s.rajarathinam1999@okaxis&pn=Raja Rathinam&am=100&cu=INR"
+        >
+          Buy Now
+        </Link> */}
+        <br />
+      </div>
+      <FloatingWhatsApp
+        accountName="SIAWED"
+        allowClickAway
+        allowEsc
+        notification={false}
+        notificationSound={false}
+      />
     </div>
   );
 };
